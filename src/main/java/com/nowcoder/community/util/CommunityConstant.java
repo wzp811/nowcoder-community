ab78196f8578320939d0f66c90e1c0f16af5b6fb
wzp <gugewu431@gmail.com>
2022-03-10 18:28:22 +0800

    Develop login and logout function.

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.util;
 
 public interface CommunityConstant {
 
+    // 邮件激活状态
     /**
      * 激活成功
      */
@@ -16,5 +17,16 @@ public interface CommunityConstant {
      * 激活失败
      */
     int ACTIVATION_FAILURE = 2;
+
+    /**
+     * 默认状态的登录凭证的超时时间
+     */
+    int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
+
+    /**
+     * 记住状态下的登录凭证超时时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
+
 
 }
