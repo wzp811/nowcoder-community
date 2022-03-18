b689b47f74a2d3459853c7c7def2a8f9a0a8b01a
wzp811 <811235852@qq.com>
2022-03-18 23:22:17 +0800

    The number of likes one received.

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -10,6 +10,7 @@ import com.nowcoder.community.util.MailClient;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -39,6 +40,9 @@ public class UserService implements CommunityConstant {
 
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     public User findUserById(int id){
         return userMapper.selectById(id);
@@ -182,5 +186,6 @@ public class UserService implements CommunityConstant {
     public User findByName(String username){
         return userMapper.selectByName(username);
     }
+
 
 }
