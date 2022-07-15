1ad0b773528fd0012a3e84618158308cd3130ea1
wzp811 <811235852@qq.com>
2022-07-15 20:23:18 +0800

    discuss post score

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,33 +36,34 @@ public class HomeController implements CommunityConstant {
     private HostHolder hostHolder;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model, Page page){
+    public String getIndexPage(Model model, Page page, @RequestParam(name = "orderMode", defaultValue = "0") int orderMode) {
         // 方法调用之前，SpringMVC会自动实例化Model和Page，并将Page注入Model
         // 所以，在thymeleaf钟可以直接访问Page对象中的数据
         page.setRows(discussPostService.findDiscussPostRows(0));
-        page.setPath("/index");
+        page.setPath("/index?orderMode=" + orderMode);
 
-        List<DiscussPost> list = discussPostService.findDiscussPosts(0,page.getOffset(),page.getLimit());
-        List<Map<String,Object>> discussPosts = new ArrayList<>();
-        if(list != null){
-            for(DiscussPost post:list){
-                Map<String,Object> map = new HashMap<>();
-                map.put("post",post);
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit(), orderMode);
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if (list != null) {
+            for (DiscussPost post : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
-                map.put("user",user);
+                map.put("user", user);
 
                 long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
-                map.put("likeCount",likeCount);
+                map.put("likeCount", likeCount);
 
                 discussPosts.add(map);
             }
         }
-        model.addAttribute("discussPosts",discussPosts);
+        model.addAttribute("discussPosts", discussPosts);
+        model.addAttribute("orderMode", orderMode);
         return "/index";
     }
 
-    @RequestMapping(path = "/error",method = RequestMethod.GET)
-    public String getErrorPage(){
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
         return "/error/500";
     }
 
--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -128,9 +127,9 @@ public class AlphaService {
         logger.debug("execute1");
     }
 
-    @Scheduled(initialDelay = 10000, fixedRate = 1000)
-    public void execute2() {
-        logger.debug("execute2");
-    }
+//    @Scheduled(initialDelay = 10000, fixedRate = 1000)
+//    public void execute2() {
+//        logger.debug("execute2");
+//    }
 
 }
