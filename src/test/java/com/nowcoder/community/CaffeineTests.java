8ee98c5590f597c5aa5e816172a6ce2b84c45143
wzp811 <811235852@qq.com>
2022-07-16 19:15:09 +0800

    caffeine cache

---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,41 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.service.DiscussPostService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService postService;
+
+    @Test
+    public void initDataForTest() {
+        for (int i = 0; i < 300000; i++) {
+            if (i % 10000 == 0) System.out.println("times: " + i);
+            DiscussPost post = new DiscussPost();
+            post.setUserId(111);
+            post.setTitle("test caffeine");
+            post.setContent("test");
+            post.setCreateTime(new Date());
+            post.setScore(Math.random() * 2000);
+            postService.addDiscussPost(post);
+        }
+    }
+
+    @Test
+    public void testCache() {
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 0));
+    }
+
+}
